9cb88b622fd6a006288b22dc2208c9acc274ce9a
Ashin Amanulla <ashin.jnv@gmail.com>
2023-03-09 00:52:17 +0530

    angular

---
+++ src/app/hero.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Product } from './interface/product';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private fs: Firestore) { }
+
+
+  //Add product data
+
+  addProduct(product: Product) {
+    product.id = doc(collection(this.fs, 'id')).id;
+    return addDoc(collection(this.fs, 'Products'), product)
+
+  }
+
+
+
+  // read data 
+  getProduct(): Observable<Product[]> {
+    let productsRef = collection(this.fs, 'Products')
+    return collectionData(productsRef, { idField: 'id' }) as Observable<Product[]>
+  }
+
+
+  // delete 
+
+  deleteProduct(product: Product) {
+    console.log(product.id)
+    let docRef = doc(this.fs, "Products",product.id)
+
+    return deleteDoc(docRef)
+  }
+
+
+  //update
+  updateProduct(id: any, updateData: any) {
+    let docRef = doc(this.fs, "Products",id)
+    return updateDoc(docRef, updateData)
+
+
+  }
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tutorial-temp-76047',
+    appId: '1:659534787550:web:a8002a0471f81f3558a34f',
+    storageBucket: 'tutorial-temp-76047.appspot.com',
+    apiKey: 'AIzaSyC7onwLnJ1ybH9duKrEuXOG2e_Y2HLNqKA',
+    authDomain: 'tutorial-temp-76047.firebaseapp.com',
+    messagingSenderId: '659534787550',
+    measurementId: 'G-RLBLH010D8',
+  },};
